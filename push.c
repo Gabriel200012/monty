af403460cbd3ac44aa023a76fbee077ea9fcaf22
Gabriel200012 <chukwuemekagabriel56@gmail.com>
2023-08-23 08:30:21 +0100

    push.c

---
+++ push.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * f_push - add node to the stack
+ * @stack - stack head
+ * 
+ * return - no return
+ */
+
+void push_stack(stack_t **stack, int n){
+	stack_t *new_code = malloc(sizeof(stack_t));
+	if (new_node == NULL){
+		fprint(stderr, "Memory allocation error\n")
+			exit(EXIT_FAILURE);
+	}
+	new_node->n = n;
+	new_node->prev =NULL;
+	new_node->next = *stack;
+
+	if(*stack !=NULL){
+		(*stack)->prev = new_node;
+	}
+	*stack = new_node;
+}
